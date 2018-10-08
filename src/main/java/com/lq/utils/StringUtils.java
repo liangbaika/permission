dfb500f5f9858827dd45cb2c4d20adfb99771302
1144388620lq <1144388620@qq.com>
2018-10-08 22:38:28 +0800

    dev

--- src/main/java/com/lq/utils/StringUtils.java
+++ src/main/java/com/lq/utils/StringUtils.java
@@ -1,6 +1,7 @@
 package com.lq.utils;
 
 import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -8,12 +9,18 @@ import java.util.stream.Collectors;
 /**
  * @Auther: LQ
  * @Date: 2018/10/8 21:59
- * @Description: 处理字符串
+ * @Description: 处理字符串  1.2.3.4
  */
 public class StringUtils {
 
     public static List<Integer> split2ListInt(String str) {
-        List<String> list = Splitter.on(",").trimResults().omitEmptyStrings().splitToList(str);
-        return list.stream().map(s -> Integer.parseInt(s)).collect(Collectors.toList());
+        try {
+            List<String> list = Splitter.on(Const.SEPARATOR_POINT).trimResults().omitEmptyStrings().splitToList(str);
+            return list.stream().map(s -> Integer.parseInt(s)).collect(Collectors.toList());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Lists.newArrayList();
+        }
+
     }
 }
